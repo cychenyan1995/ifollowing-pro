ec82aa149ec9769c0e58ba3611ad83b103bb2cc1
cy <1593225054@qq.com>
2018-12-23 12:20:56 +0800

    http.js

--- src/api/http.js
+++ src/api/http.js
@@ -28,6 +28,7 @@ axios.interceptors.response.use(
  * @param {url, params, loading} 请求地址，请求参数，是否需要加载层
  */
 let get = function (url, params) {
+  console.log(url)
   return new Promise((resolve, reject) => {
     axios
       .get(url, {
